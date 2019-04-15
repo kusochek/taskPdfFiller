09a60d0d7dfeabe1f9371b7d7093e2ba980fe2e7
Air <air@instyes-MacBook-Air.local>
2019-04-15 11:51:17 +0300

    last

--- task.js
+++ task.js
@@ -54,38 +54,38 @@ var params = {
     ]
     
 }
-for (var i = 0; i < params.lines.length; i++) {
+for (let i = 0; i < params.lines.length; i++) {
     const lines = params.lines[i].background;
 
-const maineLine = document.createElement('div');
-maineLine.classList.add('maineBlock');
-maineLine.style.backgroundColor = (lines);
-maineLine.style.height = '100%';
-maineLine.style.width = '100%';
-maineLine.style.display = 'flex';
-document.getElementById('allScreen').appendChild(maineLine);
+    const mainLine = document.createElement('div');
+    mainLine.classList.add('mainLine');
+    mainLine.style.backgroundColor = (lines);
+    mainLine.style.height = '100%';
+    mainLine.style.width = '100%';
+    mainLine.style.display = 'flex';
+    document.getElementById('allScreen').appendChild(mainLine);
 
-    for(var j = 0; j < params.lines[i].elements.length; j++) {
+    for(let j = 0; j < params.lines[i].elements.length; j++) {
         const blockColor = params.lines[i].elements[j].background;
         const blockWidth = params.lines[i].elements[j].width;
 
-    const inLine = document.createElement('div');
-    inLine.classList.add('inLine');
+        const inLine = document.createElement('div');
+        inLine.classList.add('inLine');
     
-    inLine.style.backgroundColor = blockColor;
-    inLine.style.width = (blockWidth + '%');
-    inLine.style.height = '100%';
+        inLine.style.backgroundColor = blockColor;
+        inLine.style.width = (blockWidth + '%');
+        inLine.style.height = '100%';
     
-    maineLine.appendChild(inLine);
-        function myFunction() {
+        mainLine.appendChild(inLine);
+        function randomColor() {
           
-          var letters = '0123456789ABCDEF';
-          var color = '#';
-           for(var k = 0; k < 6; k++ ) {
+            let letters = '0123456789ABCDEF';
+            let color = '#';
+            for(let k = 0; k < 6; k++ ) {
             color += letters[Math.floor(Math.random() * 16)]}
         
             inLine.style.backgroundColor = color;        
         }
-        var myVar = setInterval(function(){myFunction()},  (params.lines[i].updateTime));
+        const myVar = setInterval(function(){randomColor()},  (params.lines[i].updateTime));
     }
 }
